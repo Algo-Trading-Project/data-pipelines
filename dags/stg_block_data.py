c3e40850e5fc455a8666e2b279c028c3392906e9
Louis Spencer <louisspencer87@berkeley.edu>
2022-02-13 17:57:30 -0800

    Finished implementing custom operators and finalizing pipeline

--- dags/stg_block_data.py
+++ dags/stg_block_data.py
@@ -1,19 +1,68 @@
 from airflow import DAG
 from airflow.models import Variable
-from operators.eth_node_to_s3_operator import EthNodeToS3Operator
-import datetime
 
-with DAG('test') as dag:
-    t1 = EthNodeToS3Operator(
-        task_id = '1',
-        start_date = datetime.datetime.now(),
+from operators.web3_alchemy_to_s3_operator import Web3AlchemyToS3Operator
+from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOperator
+
+import datetime
+from web3 import Web3
+from airflow.api.client.local_client import Client
+
+with DAG('test', start_date = datetime.datetime.now(), schedule_interval = '@hourly') as dag:
+    eth_data_to_s3 = Web3AlchemyToS3Operator(
+        task_id = 'get_eth_data',
         batch_size = 1000,
         node_endpoint = Variable.get('infura_endpoint'),
         bucket_name = 'project-poseidon-data',
         region_name = 'us-west-1',
         key = 'eth_data/blocks'
     )
-    t1.execute({})
+    s3_block_data_to_redshift = S3ToRedshiftOperator(
+        task_id = 's3_block_data_to_redshift',
+        schema = 'eth_data',
+        table = 'block',
+        s3_bucket = 'project-poseidon-data',
+        s3_key = 'eth_data/blocks.json',
+        redshift_conn_id = 'redshift_conn',
+        aws_conn_id = 's3_conn',
+        copy_options = ['json']
+    )
+    s3_transaction_data_to_redshift = S3ToRedshiftOperator(
+        task_id = 's3_transaction_data_to_redshift',
+        schema = 'eth_data',
+        table = 'transaction',
+        s3_bucket = 'project-poseidon-data',
+        s3_key = 'eth_data/transactions.json',
+        redshift_conn_id = 'redshift_conn',
+        aws_conn_id = 's3_conn',
+        copy_options = ['json']
+    )
+    s3_transfer_data_to_redshift = S3ToRedshiftOperator(
+        task_id = 's3_transfer_data_to_redshift',
+        schema = 'eth_data',
+        table = 'transfer',
+        s3_bucket = 'project-poseidon-data',
+        s3_key = 'eth_data/transfers.json',
+        redshift_conn_id = 'redshift_conn',
+        aws_conn_id = 's3_conn',
+        copy_options = ['json']
+    )
+    
+    eth_data_to_s3 >> [s3_block_data_to_redshift, s3_transaction_data_to_redshift, s3_transfer_data_to_redshift]
+    
+    # s3_block_data_to_redshift.execute({})
+    # print('s3 block data is in redshift')
+    # print()
 
-    # sudo lsof -i tcp:8080
-    # kill -9 408103312\\
+    # s3_block_data_to_redshift.execute({})
+    # print('s3 transaction data is in redshift')
+    # print()
+
+    # s3_block_data_to_redshift.execute({})
+    # print('s3 transfer data is in redshift')
+    # print()
+
+    # print('dag run completed!!!!')
+
+#     # sudo lsof -i tcp:8080
+#     # kill -9 25092.\\
---
+++ plugins/operators/web3_alchemy_to_s3_operator.py
@@ -0,0 +1,196 @@
+from distutils.command.config import config
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.providers.amazon.aws.hooks.base_aws import AwsBaseHook
+from airflow.models import BaseOperator
+from airflow.models import Variable
+from botocore.config import Config
+
+from web3 import Web3
+
+import requests as r
+import json
+
+class Web3AlchemyToS3Operator(BaseOperator):
+
+    def __init__(self, batch_size, node_endpoint, bucket_name, region_name, key, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.batch_size = batch_size
+
+        self.node_endpoint = node_endpoint
+        self.web3_instance = None
+
+        self.bucket_name = bucket_name
+        self.region_name = region_name
+        self.key = key
+        self.s3_connection = None
+
+    ############################ HELPER FUNCTIONS ##########################################
+    def __set_up_connections(self):
+        web_provider = Web3.HTTPProvider(self.node_endpoint)
+        self.web3_instance = Web3(web_provider)
+
+        self.s3_connection = S3Hook(
+            aws_conn_id = 's3_conn'
+        )
+
+    def __get_transfer_data(self):
+        params = {
+            'jsonrpc':'2.0',
+            'id':0,
+            'method':'alchemy_getAssetTransfers',
+            'params': [{
+                'fromBlock':'0x' + hex(int(Variable.get('start_block')))[2:].upper(),
+                'toBlock':'0x' + hex(int(Variable.get('end_block')))[2:].upper(),
+                'category': [
+                    'erc20', 'external', 'internal'
+                ]
+            }]
+        }
+        headers = {'Content-Type':'application/json'}
+
+        response = r.post(
+            url = Variable.get('alchemy_api_endpoint'),
+            headers = headers,
+            data = json.dumps(params)
+        )
+        
+        preprocessed_transfers = response.json()['result']['transfers']
+        processed_transfers = []
+
+        for preprocessed_transfer in preprocessed_transfers:
+            processed_transfer = {}
+            
+            processed_transfer['transaction_hash'] = preprocessed_transfer['hash']
+            processed_transfer['block_no'] = int(preprocessed_transfer['blockNum'], 0)
+            processed_transfer['from'] = preprocessed_transfer['from']
+            processed_transfer['to'] = preprocessed_transfer['to']
+            processed_transfer['token_units'] = preprocessed_transfer['value']
+            processed_transfer['raw_token_units'] = int(preprocessed_transfer['rawContract']['value'], 0)
+            processed_transfer['asset'] = preprocessed_transfer['asset']
+            processed_transfer['category'] = preprocessed_transfer['category']
+            processed_transfer['token_address'] = preprocessed_transfer['rawContract']['address']
+
+            processed_transfers.append(processed_transfer)
+
+        return processed_transfers
+
+    def __get_block_data(self):
+        start_block = int(Variable.get('start_block'))
+        end_block = int(Variable.get('end_block'))
+        
+        transaction_batch = []
+        block_batch = []
+
+        preprocessed_blocks = [self.web3_instance.eth.get_block(i, full_transactions = True) for i in range(start_block, end_block + 1)]
+        
+        for block in preprocessed_blocks:
+            processed_block_data = {}
+            
+            processed_block_data['block_no'] = block['number']
+            processed_block_data['hash'] = block['hash'].hex()
+            processed_block_data['parent_hash'] = block['parentHash'].hex()
+            processed_block_data['timestamp'] = block['timestamp']
+            processed_block_data['difficulty'] = block['difficulty']
+            processed_block_data['miner'] = block['miner']
+            processed_block_data['gas_used'] = block['gasUsed']
+            processed_block_data['size'] = block['size']
+            processed_block_data['sha3uncles'] = block['sha3Uncles'].hex()
+            processed_block_data['gas_limit'] = block['gasLimit']
+
+            block_batch.append(processed_block_data)
+            transaction_batch.append(block['transactions'])
+
+        return block_batch, transaction_batch
+
+    def __get_transaction_method(self, transaction):
+        if self.web3_instance.eth.get_code(transaction['to']) == '0x':
+            return 'Transfer'
+
+        tx = self.web3_instance.eth.get_transaction(transaction['transaction_hash'])
+        etherscan_api_key = Variable.get('etherscan_api_key')
+        abi_endpoint = "https://api.etherscan.io/api?module=contract&action=getabi&address={}&apikey={}".format(transaction['to'], etherscan_api_key)
+        
+        abi = json.loads(r.get(abi_endpoint).text)
+        try:
+            contract = self.web3_instance.eth.contract(address = transaction['to'], abi = abi["result"])
+            func_obj, func_params = contract.decode_function_input(tx["input"])
+            return func_obj.functions.name().call()
+        except Exception as e:
+            return None
+
+    def __get_transaction_data(self, transaction_batch):
+        preprocessed_transactions = []
+        processed_transactions = []
+
+        for transaction in transaction_batch:
+            preprocessed_transactions.extend(transaction)
+
+        for transaction in preprocessed_transactions:
+            processed_transaction = {}
+
+            processed_transaction['transaction_hash'] = transaction['hash'].hex()
+            processed_transaction['block_no'] = transaction['blockNumber']
+            processed_transaction['to'] = transaction['to']
+            processed_transaction['from'] = transaction['from']
+            processed_transaction['value'] = transaction['value']
+            processed_transaction['gas'] = transaction['gas']
+            processed_transaction['gas_price'] = transaction['gasPrice']
+
+            # processed_transaction['transaction_method'] = self.__get_transaction_method(processed_transaction)
+            
+            processed_transactions.append(processed_transaction)
+            
+        return processed_transactions
+
+    def __upload_to_s3(self, block_data, transaction_data, transfer_data):
+        self.s3_connection.load_string(
+            json.dumps(block_data),
+            key = 'eth_data/blocks.json',
+            bucket_name = self.bucket_name,
+            replace = True
+        )
+
+        self.s3_connection.load_string(
+            json.dumps(transaction_data),
+            key = 'eth_data/transactions.json',
+            bucket_name = self.bucket_name,
+            replace = True
+        )
+
+        self.s3_connection.load_string(
+            json.dumps(transfer_data),
+            key = 'eth_data/transfers.json',
+            bucket_name = self.bucket_name,
+            replace = True
+        )
+
+    ############################ HELPER FUNCTIONS END ######################################
+
+    def execute(self, context):
+        self.__set_up_connections()
+        print('finished setting up connections...')
+        print()
+        block_data, transaction_batch = self.__get_block_data()
+        print('finished getting block data...')
+        print()
+        transaction_data = self.__get_transaction_data(transaction_batch)
+        print('finished getting transaction data...')
+        print()
+        transfer_data = self.__get_transfer_data()
+        print('finished getting transfer data...')
+        print()
+
+        self.__upload_to_s3(block_data, transaction_data, transfer_data)
+        print('upload to s3 went through...')
+        print()
+        # Variable.set(
+        #     key = 'start_block',
+        #     value = str(int(Variable.get('end_block')) + 1)
+        # )
+        # Variable.set(
+        #     key = 'end_block',
+        #     value = str(self.web3_instance.eth.block_number)
+        # )
+
+        
