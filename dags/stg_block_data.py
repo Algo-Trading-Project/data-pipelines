9eea887992f0de75525cb8300ae658a90146f010
Louis Spencer <louisspencer87@berkeley.edu>
2022-03-09 17:14:19 -0800

    changed start date for get_eth_block_and_transaction_data pipeline

--- dags/stg_block_data.py
+++ dags/stg_block_data.py
@@ -8,7 +8,7 @@ import pytz
 
 timezone = pytz.timezone('America/Los_Angeles')
 schedule_interval = timedelta(minutes = 15)
-start_date = timezone.localize(datetime(year = 2022, month = 3, day = 9))
+start_date = timezone.localize(datetime(year = 2022, month = 3, day = 9, hour = 6))
 
 with DAG('get_eth_block_and_transaction_data', start_date = start_date, schedule_interval = schedule_interval) as dag:
     eth_data_to_s3 = Web3AlchemyToS3Operator(
