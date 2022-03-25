8b8dcd8185eb25405ea4889bc32b0b8bd5764b61
Louis Spencer <louisspencer87@berkeley.edu>
2022-03-25 16:21:52 -0700

    fixed bug

--- plugins/operators/web3_alchemy_to_s3_operator.py
+++ plugins/operators/web3_alchemy_to_s3_operator.py
@@ -28,12 +28,8 @@ class Web3AlchemyToS3Operator(BaseOperator):
 
     ############################ HELPER FUNCTIONS ##########################################
     def __get_start_and_end_block(self):
-        print('var', Variable(self.start_block_variable_name))
-        print(type(Variable(self.start_block_variable_name)))
-        start_block = str(Variable(self.start_block_variable_name))
-        start_block = int(start_block)
-        end_block = str(Variable(self.end_block_variable_name))
-        end_block = int(end_block)
+        start_block = Variable.get(self.start_block_variable_name)
+        end_block = Variable.get(self.end_block_variable_name)
 
         return start_block, end_block
 
