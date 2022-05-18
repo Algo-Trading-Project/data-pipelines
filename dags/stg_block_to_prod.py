c1050b8911bcf1f5d492ba4f2b2cb004253c1825
Louis Spencer <louisspencer87@berkeley.edu>
2022-05-17 19:26:38 -0700

    deleted RedshiftToRedshiftOperator, replaced it with RedshiftSQLOperator, and created stg_block_to_prod pipeline

---
+++ dags/stg_block_to_prod.py
@@ -0,0 +1,48 @@
+from operators.redshift_to_redshift_operator import RedshiftToRedshiftOperator
+from operators.redshift_sql_operator import RedshiftSQLOperator
+from datetime import timedelta
+
+from airflow import DAG
+
+schedule_interval = timedelta(days = 1)
+start_date = pendulum.datetime(year = 2022,
+                               month = 5,
+                               day = 17,
+                               tz = 'America/Los_Angeles')
+
+with DAG(
+    dag_id = 'stg_block_to_prod',
+    schedule_interval = schedule_interval,
+    start_date = start_date,
+    catchup = False,
+    max_active_runs = 1
+) as dag:
+    move_staging_data_to_prod = RedshiftSQLOperator(
+        task_id = 'delete_old_data_from_staging',
+        query = """
+        BEGIN;
+
+        INSERT INTO administrator.eth_data.block (
+            SELECT *
+            FROM 
+                eth_data.stg_block
+            WHERE 
+                date >= (
+                    SELECT MAX(date)
+                    FROM eth_data.block
+                )
+        );
+
+        DELETE FROM administrator.eth_data.stg_block
+        WHERE 
+            date >= (
+                SELECT MAX(date)
+                FROM administrator.eth_data.block
+            );
+
+        COMMIT;
+        """
+    )
+
+    move_staging_data_to_prod 
+
--- plugins/operators/redshift_sql_operator.py
+++ plugins/operators/redshift_sql_operator.py
@@ -2,28 +2,18 @@ from airflow.models import BaseOperator
 import redshift_connector
 from airflow.models import Variable
 
-class RedshiftToRedshiftOperator(BaseOperator):
+class RedshiftSQLOperator(BaseOperator):
 
-    def __init__(self, database, schema, table, query):
+    def __init__(self, query):
         self.query = query
         
-        self.database = database
-        self.schema = schema
-        self.table = table
-
     def execute(self, context):
         with redshift_connector.connect(
             host = Variable.get('redshift_host'),
-            database = 'administrator',
             user = 'administrator',
             password = Variable.get('redshift_password')
         ) as conn:
             with conn.cursor() as cursor:
-                query = """
-                INSERT INTO {}.{}.{} VALUES (
-                    ({})
-                )
-                """.format(self.database, self.schema, self.table, self.query)
-                cursor.execute(query)
+                cursor.execute(self.query)
                 conn.commit()
                         
