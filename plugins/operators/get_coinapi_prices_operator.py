f835531fdb599bb1eb0d1835e2ed923d31e0c5c7
Louis Spencer <louisspencer87@berkeley.edu>
2022-06-01 22:07:20 -0700

    small changes

--- plugins/operators/get_coinapi_prices_operator.py
+++ plugins/operators/get_coinapi_prices_operator.py
@@ -1,5 +1,4 @@
 from airflow.models import BaseOperator
-from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOperator
 
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.models import Variable
@@ -7,7 +6,6 @@ from airflow.models import Variable
 import requests as r
 import json
 import pandas as pd
-import datetime
 import dateutil.parser as parser
 
 class GetCoinAPIPricesOperator(BaseOperator):
