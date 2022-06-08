b5963469322c72bbb686adebd19d47c9e5efbfef
Louis Spencer <louisspencer87@berkeley.edu>
2022-06-08 13:01:10 -0700

    added pipeline for 1 hour price data for ETH pairs named fetch_token_prices_1h

--- dags/fetch_token_prices_1h.py
+++ dags/fetch_token_prices_1h.py
@@ -4,47 +4,47 @@ from airflow.models import Variable
 from operators.get_coinapi_prices_operator import GetCoinAPIPricesOperator
 
 from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOperator
+from airflow.operators.python import PythonOperator
 
 from datetime import timedelta
 import pendulum
 
-
 schedule_interval = timedelta(days = 1)
 start_date = pendulum.datetime(year = 2022,
-                               month = 5,
-                               day = 28,
-                               hour = 7,
+                               month = 6,
+                               day = 8,
+                               hour = 2,
                                tz = 'America/Los_Angeles')
 
 with DAG(
-    dag_id = 'fetch_token_prices_1d',
-    schedule_interval = schedule_interval,
+    dag_id = 'fetch_token_prices_1h',
     start_date = start_date,
-    catchup = False,
-    max_active_runs = 1
+    schedule_interval = schedule_interval,
+    max_active_runs =  1,
+    catchup = False
 ) as dag:
-    eth_pairs_1d_price_data_to_s3 = GetCoinAPIPricesOperator(
-        task_id = 'eth_pairs_1d_price_data_to_s3',
-        time_interval = 'day'
+    eth_pairs_1h_price_data_to_s3 = GetCoinAPIPricesOperator(
+        task_id = 'eth_pairs_1h_price_data_to_s3',
+        time_interval = 'hour'
     )
 
-    price_data_1d_cols = ['time_period_start', 'time_period_end', 'time_open', 'time_close',
+    price_data_1h_cols = ['time_period_start', 'time_period_end', 'time_open', 'time_close',
                           'price_open', 'price_high', 'price_low', 'price_close', 'volume_traded',
                           'trades_count', 'exchange_id', 'symbol_id', 'asset_id_base', 
                           'asset_id_quote']
 
-    s3_eth_pairs_1d_price_data_to_redshift = S3ToRedshiftOperator(
-        task_id = 's3_eth_pairs_1d_price_data_to_redshift',
+    s3_eth_pairs_1h_price_data_to_redshift = S3ToRedshiftOperator(
+        task_id = 's3_eth_pairs_1h_price_data_to_redshift',
         schema = 'eth',
-        table = 'price_data_1d',
+        table = 'price_data_1h',
         s3_bucket = 'project-poseidon-data',
-        s3_key = 'eth_data/price_data/coinapi_pair_prices_1_day.json',
+        s3_key = 'eth_data/price_data/coinapi_pair_prices_1_hour.json',
         redshift_conn_id = 'token_price_database_conn',
         aws_conn_id = 's3_conn',
         method = 'UPSERT',
         upsert_keys = ['exchange_id', 'asset_id_base', 'asset_id_quote', 'time_period_start'],
         copy_options = ["json 'auto'", "TIMEFORMAT 'auto'"],
-        column_list = price_data_1d_cols
+        column_list = price_data_1h_cols
     )
 
-    eth_pairs_1d_price_data_to_s3 >> s3_eth_pairs_1d_price_data_to_redshift
+    eth_pairs_1h_price_data_to_s3 >> s3_eth_pairs_1h_price_data_to_redshift
