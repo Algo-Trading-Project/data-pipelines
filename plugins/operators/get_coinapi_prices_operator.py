94f3f267bc84427b564ceacbf763ce7cafc1f80c
Louis <louisspencer87@gmail.com>
2023-12-24 10:04:40 -0800

    Added documentation & token filter to GetCoinAPIPricesOperator

--- plugins/operators/get_coinapi_prices_operator.py
+++ plugins/operators/get_coinapi_prices_operator.py
@@ -9,6 +9,25 @@ import pandas as pd
 import dateutil.parser as parser
 
 class GetCoinAPIPricesOperator(BaseOperator):
+    """
+    An Airflow operator to fetch and update Ethereum (ETH) token price data from CoinAPI,
+    and manage related metadata.
+
+    It performs several key functions:
+    - Fetches latest price data from CoinAPI for different ETH pairs.
+    - Uploads this data to a specified AWS S3 bucket.
+    - Updates and manages the metadata associated with each ETH pair.
+    - Handles various API response statuses and potential errors.
+    """
+
+    DESIRED_TOKENS = [
+        'BTC_USD_COINBASE', 'ETH_USD_COINBASE', 'ADA_USDT_BINANCE',
+        'ALGO_USD_COINBASE', 'ATOM_USDT_BINANCE', 'BCH_USD_COINBASE',
+        'BNB_USDC_BINANCE', 'DOGE_USDT_BINANCE', 'ETC_USD_COINBASE',
+        'FET_USDT_BINANCE', 'FTM_USDT_BINANCE', 'HOT_USDT_BINANCE',
+        'IOTA_USDT_BINANCE', 'LINK_USD_COINBASE', 'LTC_USD_COINBASE',
+        'MATIC_USDT_BINANCE',
+    ]
     
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -17,6 +36,16 @@ class GetCoinAPIPricesOperator(BaseOperator):
         self.s3_file_chunk_num = 1
 
     def __get_next_start_date(self, coinapi_pair):
+        """
+        Determines the next start date for fetching price data.
+
+        Parameters:
+        coinapi_pair: A dictionary containing information about the ETH pair.
+
+        Returns:
+        A string representing the next start date in ISO format.
+        """
+
         most_recent_data_date = coinapi_pair['latest_scrape_date_price']
         next_start_date = None
 
@@ -28,6 +57,13 @@ class GetCoinAPIPricesOperator(BaseOperator):
         return next_start_date
 
     def __upload_new_price_data(self, new_price_data):
+        """
+        Uploads new price data to an S3 bucket.
+
+        Parameters:
+        new_price_data: A list of dictionaries containing the new price data.
+        """
+
         data_to_uplaod = json.dumps(new_price_data).replace('[', '').replace(']', '').replace('},', '}')
         key = 'eth_data/price_data/coinapi_pair_prices_1_hour.json.{}'.format(self.s3_file_chunk_num)
 
@@ -41,6 +77,13 @@ class GetCoinAPIPricesOperator(BaseOperator):
         self.s3_file_chunk_num += 1
 
     def __upload_new_coinapi_eth_pairs_metadata(self, coinapi_pairs_df):
+        """
+        Uploads the metadata of CoinAPI ETH pairs to an S3 bucket.
+
+        Parameters:
+        coinapi_pairs_df: A DataFrame containing the metadata of CoinAPI ETH pairs.
+        """
+
         coinapi_pairs_df_json = coinapi_pairs_df.to_dict(orient = 'records')
         coinapi_pairs_str = json.dumps(coinapi_pairs_df_json)
 
@@ -52,6 +95,18 @@ class GetCoinAPIPricesOperator(BaseOperator):
         )
 
     def __update_coinapi_pairs_metadata(self, latest_price_data_for_pair, coinapi_pairs_df, coinapi_pair):
+        """
+        Updates the metadata DataFrame for a specific ETH pair.
+
+        Parameters:
+        latest_price_data_for_pair: Latest price data for the ETH pair.
+        coinapi_pairs_df: DataFrame containing metadata for all ETH pairs.
+        coinapi_pair: Information about the specific ETH pair.
+
+        Returns:
+        Updated DataFrame with the new metadata for the ETH pairs.
+        """
+
         asset_id_base = coinapi_pair['asset_id_base']
         asset_id_quote = coinapi_pair['asset_id_quote']
         exchange_id = coinapi_pair['exchange_id']
@@ -68,6 +123,10 @@ class GetCoinAPIPricesOperator(BaseOperator):
         return coinapi_pairs_df
     
     def __delete_price_data_from_s3(self):
+        """
+        Deletes price data files from the S3 bucket.
+        """
+
         keys_to_delete = self.s3_connection.list_keys(
             bucket_name = 'project-poseidon-data',
             prefix = 'eth_data/price_data'
@@ -79,6 +138,17 @@ class GetCoinAPIPricesOperator(BaseOperator):
         )
 
     def __get_latest_price_data(self, coinapi_symbol_id, time_start):
+        """
+        Fetches the latest price data from CoinAPI.
+
+        Parameters:
+        coinapi_symbol_id: The symbol ID used in CoinAPI to identify the ETH pair.
+        time_start: The start time from which to fetch the price data.
+
+        Returns:
+        Formatted response data as a list of dictionaries, or an error code as an integer.
+        """
+
         def format_response_data(response):
             exchange_id, symbol_id, asset_id_base, asset_id_quote = coinapi_symbol_id.split('_')
             
@@ -140,6 +210,17 @@ class GetCoinAPIPricesOperator(BaseOperator):
             return -1
         
     def execute(self, context):
+        """
+        The main execution function of the operator. 
+
+        It orchestrates the entire process of data fetching, processing, and updating.
+        This involves deleting existing price data from S3, reading and updating metadata
+        for ETH pairs, and fetching new price data for each pair, followed by uploading it to S3.
+
+        Parameters:
+        context: Airflow's execution context object.
+        """
+
         # Delete price data potentially stored in S3 from previous task runs
         self.__delete_price_data_from_s3()
 
@@ -150,6 +231,11 @@ class GetCoinAPIPricesOperator(BaseOperator):
         coinapi_pairs_str = self.s3_connection.read_key(key = key, bucket_name = 'project-poseidon-data')
         coinapi_pairs_json = json.loads(coinapi_pairs_str)
         coinapi_pairs_df = pd.DataFrame(coinapi_pairs_json)
+
+        # Filter out tokens we don't want to get price data for
+        base_quote_exchange = coinapi_pairs_df['asset_id_base'] + '_' + coinapi_pairs_df['asset_id_quote'] + '_' + coinapi_pairs_df['exchange_id']
+        predicate = base_quote_exchange.isin(self.DESIRED_TOKENS)
+        coinapi_pairs_df = coinapi_pairs_df[predicate]
 
         # For each token we have metadata for
         for i in range(len(coinapi_pairs_df)):
