8e9bd673410d9476953036cab760ce1f838e7350
Louis <louisspencer87@gmail.com>
2024-05-21 20:54:32 -0700

    changed name of dag file and task name for fetching 1m price data

--- dags/fetch_price_data_1m.py
+++ dags/fetch_price_data_1m.py
@@ -13,21 +13,15 @@ start_date = pendulum.datetime(
 )
 
 with DAG(
-    dag_id = 'fetch_price_data_1h',
+    dag_id = 'fetch_price_data_1m',
     start_date = start_date,
     schedule_interval = schedule_interval,
     max_active_runs =  1,
     catchup = False
 ) as dag:
     
-    eth_pairs_1h_price_data_to_s3 = GetCoinAPIPricesOperator(
-        task_id = 'eth_pairs_1h_price_data_to_s3'
+    price_data_1m_to_duck_db = GetCoinAPIPricesOperator(
+        task_id = 'price_data_1m_to_duck_db'
     )
 
-    price_data_1h_cols = [
-        'time_period_start', 'time_period_end', 'time_open', 'time_close',
-        'price_open', 'price_high', 'price_low', 'price_close', 'volume_traded',
-        'trades_count', 'exchange_id', 'asset_id_base', 'asset_id_quote'
-    ]
-
-    eth_pairs_1h_price_data_to_s3
+    price_data_1m_to_duck_db
