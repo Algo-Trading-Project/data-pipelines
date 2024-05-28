afc4ed54ad34916f284084f5acdcf2c141578fd7
Louis <louisspencer87@gmail.com>
2024-05-28 10:59:09 -0700

    fixing

---
+++ dags/fetch_price_data_1m.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from operators.get_coinapi_prices_operator import GetCoinAPIPricesOperator
+
+from datetime import timedelta
+import pendulum
+
+schedule_interval = timedelta(days = 1, hours = 1)
+start_date = pendulum.datetime(
+    year = 2023,
+    month = 8,
+    day = 15,
+    tz = 'America/Los_Angeles'
+)
+
+with DAG(
+    dag_id = 'fetch_price_data_1m',
+    start_date = start_date,
+    schedule_interval = schedule_interval,
+    max_active_runs =  1,
+    catchup = False
+) as dag:
+    
+    price_data_1m_to_duck_db = GetCoinAPIPricesOperator(
+        task_id = 'price_data_1m_to_duck_db'
+    )
+
+    price_data_1m_to_duck_db
